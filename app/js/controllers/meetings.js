e61f28fd36eaae076b7f614137e842b42684d80e
nannen05 <nannen05@gmail.com>
2016-03-03 15:07:35 -0800

    added in new meetings to db

--- app/js/controllers/meetings.js
+++ app/js/controllers/meetings.js
@@ -1,3 +1,23 @@
-myApp.controller('MeetingsController' , ['$scope', 'Authentication', function($scope, Authentication) {
-	//$scope.message = "Success";
+myApp.controller('MeetingsController' , ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
+ function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+	
+ 	var ref = new Firebase(FIREBASE_URL);
+ 	var auth = $firebaseAuth(ref);
+
+ 	auth.$onAuth(function(authUser) {
+ 		if (authUser) {
+ 			var meetingsRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/meetings');
+ 			var meetingsInfo = $firebaseArray(meetingsRef);
+
+ 			$scope.addMeeting = function() {
+ 				meetingsInfo.$add({
+ 					name: $scope.meetingname,
+ 					date: Firebase.ServerValue.TIMESTAMP
+ 				}).then(function() {
+ 					$scope.meetingname = '';
+ 				}); // promise
+ 			} // addMeeting
+ 		} // User Authenticated
+ 	});// on Auth 
+
 }]);
